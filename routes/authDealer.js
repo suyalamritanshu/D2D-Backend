9309268de608fd00963675049e0892f6f881f708
suyalamritanshu <amritanshu.suyall2@gmail.com>
2022-02-14 20:37:11 +0530

    Fixes

--- routes/authDealer.js
+++ routes/authDealer.js
@@ -32,29 +32,31 @@ router.post("/registerDealer", async (req, res) => {
 
 //login
 router.post("/loginDealer", async (req, res) => {
-    try {
-      const user = await User.findOne({ email: req.body.email });
-      !user && res.status(401).json("Wrong password or email!");
-  
-      const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
-      const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
-  
-      originalPassword !== req.body.password &&
-        res.status(401).json("Wrong password or email!");
-  
-      const accessToken = jwt.sign(
-        { id: user._id },
-        process.env.SECRET_KEY,
-        { expiresIn: "1d" }
-      );
-  
-      const { password, ...info } = user._doc;
-  
-      res.status(200).json({ ...info, accessToken });
-    } catch (err) {
-      res.status(500).json(err);
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(401).json("Wrong password or username!");
+
+    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+    if(originalPassword !== req.body.password){
+     
+      return res.status(401).json("Wrong password");
     }
-  });
+
+    const accessToken = jwt.sign(
+      { id: user._id },
+      process.env.SECRET_KEY,
+      { expiresIn: "1d" }
+    );
+
+    const { password, ...info } = user._doc;
+
+    res.status(200).json({ ...info, accessToken });
+  } catch (err) {
+    res.status(401).json('Wrong password');
+  }
+});
   
 //get all dealers for a particular route
 
